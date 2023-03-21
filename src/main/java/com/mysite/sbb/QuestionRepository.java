af189e919e8d54f192a97cea7f8cbb2ebdc55a5e
debussysuh <debussysuh@gmail.com>
2023-03-21 11:03:15 +0900

    refactor: 각 테스트 케이스 독립성 확보

--- src/main/java/com/mysite/sbb/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -8,4 +9,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer > {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+
+    @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
+    void clearAutoIncrement();
 }
