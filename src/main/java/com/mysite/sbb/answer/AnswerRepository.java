f3a267cea62a0e6e8f35b0650dd8f0ddc7c4a5fe
debussysuh <debussysuh@gmail.com>
2023-03-22 15:44:21 +0900

    관련 답변 표시하도록 수정

--- src/main/java/com/mysite/sbb/answer/AnswerRepository.java
+++ src/main/java/com/mysite/sbb/answer/AnswerRepository.java
@@ -4,8 +4,12 @@ import com.mysite.sbb.answer.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface AnswerRepository extends JpaRepository<Answer, Integer> {
 
     @Query(value = "ALTER TABLE answer AUTO_INCREMENT = 1", nativeQuery = true)
     void clearAutoIncrement();
+
+    List<Answer> findByQuestionId(Integer id);
 }
