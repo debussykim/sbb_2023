26bcbe0a838b50735194baf62774f8034fb90712
debussysuh <debussysuh@gmail.com>
2023-03-22 09:26:08 +0900

    Rollback(false)

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -209,6 +210,7 @@ class SbbApplicationTests {
 	}
 
 	@Transactional
+	@Rollback(value = false)
 	@Test
 	@DisplayName("질문에 달린 답변 찾기")
 	void t011() {
