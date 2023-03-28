528a20f0f809860619c9ee908ea8a8b85e761d7d
debussysuh <debussysuh@gmail.com>
2023-03-28 11:50:12 +0900

    회원가입 폼 추가 및 처리

--- src/main/java/com/mysite/sbb/user/UserService.java
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -2,12 +2,13 @@ package com.mysite.sbb.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
-
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
     public SiteUser create(String username, String email, String password) {
