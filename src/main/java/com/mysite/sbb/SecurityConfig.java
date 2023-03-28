8ed560ac2e73f81f3d5836167a59443405a56b66
JH <118197952+debussykim@users.noreply.github.com>
2023-03-28 15:28:55 +0900

    #3 Feat/3 로그인 검증, 로그인 구현 완료 (#4)
    * test: 테스트 코드 추가
    
    * #3 로그인 없이 접속할 수 있도록 설정

--- src/main/java/com/mysite/sbb/SecurityConfig.java
+++ src/main/java/com/mysite/sbb/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -14,12 +15,15 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                /*
                 .authorizeHttpRequests(
                         authorizeHttpRequests -> authorizeHttpRequests
                                 .requestMatchers(new AntPathRequestMatcher("/question/list")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/question/detail/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/user/login")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/style.css")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                                .anyRequest().authenticated()
                 )
-                */
                 .formLogin(
                         formLogin -> formLogin
                                 .loginPage("/user/login")
