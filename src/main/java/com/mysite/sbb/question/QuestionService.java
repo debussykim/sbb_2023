7f0dbcd7c80d7eaab309efb29c846007412bb88b
debussysuh <debussysuh@gmail.com>
2023-03-29 11:17:22 +0900

    #10 질문 삭제 완료

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -53,5 +53,9 @@ public class QuestionService {
         question.setModifyDate(LocalDateTime.now());
         questionRepository.save(question);
     }
+
+    public void delete(Question question) {
+        questionRepository.delete(question);
+    }
 }
 
