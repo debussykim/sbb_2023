73c0630febed9108c166452448cfd8b0cc5beab1
debussysuh <debussysuh@gmail.com>
2023-04-02 11:27:06 +0900

    #18 feat: Specification을 이용한 검색 기능 구현

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -1,5 +1,8 @@
 package com.mysite.sbb.question;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +14,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer > {
     Question findBySubject(String subject);
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
+    Page<Question> findAll(Specification<Question> spce, Pageable pageable);
 
     @Transactional
     @Modifying
